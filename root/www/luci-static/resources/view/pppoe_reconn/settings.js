c9ef5ab9eeb090a2ed2474e87e3abeed1745927b
Chinjunmin <14328288+Chinjunmin@users.noreply.github.com>
2026-01-04 23:01:37 +0800

    update

--- root/www/luci-static/resources/view/pppoe_reconn/settings.js
+++ root/www/luci-static/resources/view/pppoe_reconn/settings.js
@@ -6,18 +6,21 @@ return view.extend({
     render: function () {
         var m, s, o;
 
-        m = new form.Map('pppoe_reconn', _('PPPoE 重拨设置'),
-            _('配置 PPPoE 断开等待时间,保存后自动生效.')
+        m = new form.Map(
+            'pppoe_reconn',
+            _('PPPoE 重拨设置'),
+            _('配置 PPPoE 断开等待时间，保存后自动生效。')
         );
 
-        s = m.section(form.TypedSection, 'main', '');
-        s.anonymous = true;
+        // 固定 section：main
+        s = m.section(form.NamedSection, 'main', 'main', '');
+        s.addremove = false;
 
         o = s.option(form.Value, 'delay', _('断开等待时长（秒）'));
         o.datatype = 'uinteger';
         o.default = '10';
         o.placeholder = '10';
-        o.description = _('默认设置10秒,如不能正确切换IP,请增加时长以确保运营商机房识别下线.');
+        o.description = _('默认 10 秒，如无法成功更换 IP，请增加时长以确保运营商识别下线。');
 
         return m.render();
     }
